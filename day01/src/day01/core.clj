43a077e6b680f1b7717c409242eb69c1d45641de
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-07-25 21:57:11 +0300

    day1 p1: compute solution

--- day01/src/day01/core.clj
+++ day01/src/day01/core.clj
@@ -3,8 +3,26 @@
 
 (def input-file "resources\\input.txt")
 
-(def directions (slurp input-file))
+(def instructions (slurp input-file))
+
+; --------------------------
+; problem 1
+
+(defn find-final-floor
+  "Finds santa's final floor"
+  []
+  (let [steps (group-by identity instructions)
+        up-count (count (get steps \())
+        down-count (count (get steps \)))]
+    (- up-count down-count)))
+
+; ---------------------------------------
+; results
+
+(defn day01-1
+  []
+  (find-final-floor))
 
 (defn -main
   []
-  (println directions))
+  (println (day01-1)))
