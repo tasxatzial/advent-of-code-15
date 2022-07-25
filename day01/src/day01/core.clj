dba521e1dd2a45399fec665188071321472523fc
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-07-25 22:21:47 +0300

    day1 p2: compute solution

--- day01/src/day01/core.clj
+++ day01/src/day01/core.clj
@@ -9,13 +9,29 @@
 ; problem 1
 
 (defn find-final-floor
-  "Finds santa's final floor"
+  "Finds santa's final floor."
   []
   (let [steps (group-by identity instructions)
         up-count (count (get steps \())
         down-count (count (get steps \)))]
     (- up-count down-count)))
 
+; --------------------------
+; problem 2
+
+(defn find-first-negative
+  "Finds the position of the first instruction that causes santa to enter the basement."
+  []
+  (loop [[instruction & rest-instructions] instructions
+         instruction-index 0
+         floor 0]
+    (if instruction
+      (if (= -1 floor)
+        instruction-index
+        (if (= \( instruction)
+          (recur rest-instructions (inc instruction-index) (inc floor))
+          (recur rest-instructions (inc instruction-index) (dec floor))))
+      -1)))
 ; ---------------------------------------
 ; results
 
@@ -23,6 +39,11 @@
   []
   (find-final-floor))
 
+(defn day01-2
+  []
+  (find-first-negative))
+
 (defn -main
   []
-  (println (day01-1)))
+  (println (day01-1))
+  (println (day01-2)))
