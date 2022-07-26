4ba43167b91729538d5748f54bb96618d5b58344
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-07-26 19:11:36 +0300

    day5 p1: find if a string is nice

--- day05/src/day05/core.clj
+++ day05/src/day05/core.clj
@@ -13,9 +13,41 @@
 
 (def strings (parse (slurp input-file)))
 
+(defn is-vowel?
+  [c]
+  (or (= c \a) (= c \e) (= c \i) (= c \o) (= c \u)))
+
+; --------------------------
+; problem 1
+
+(defn prop1-p1?
+  "Returns true if the given string has at least 3 vowels, false otherwise."
+  [s]
+  (>= (count (filter is-vowel? s)) 3))
+
+(defn prop2-p1?
+  "Returns true if the given string has least one letter that appears twice in a row,
+  false otherwise."
+  [s]
+  (boolean (re-find #"([a-z])\1" s)))
+
+(defn prop3-p1?
+  "Returns true if the given string does not contain ab, cd, pq, xy, false otherwise."
+  [s]
+  (->> ["ab" "cd" "pq" "xy"]
+       (mapv #(re-find (re-pattern %) s))
+       (every? nil?)))
+
+(defn nice-string?
+  "Returns true if the given string is nice, false otherwise."
+  [s]
+  (and (prop1-p1? s)
+       (prop2-p1? s)
+       (prop3-p1? s)))
+
 ; --------------------------
 ; results
 
 (defn -main
   []
-  (println strings))
+  (println (nice-string? "aaa")))
