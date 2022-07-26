91b20d715cfe01291157b1bb97f1b322de3144d1
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-07-26 11:42:40 +0300

    day2: parse input

--- day02/src/day02/core.clj
+++ day02/src/day02/core.clj
@@ -1,7 +1,30 @@
 (ns day02.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse-line
+  "Parse an input line and return a vector of 3 integers
+  that denote the gift dimensions."
+  [s]
+  (let [split-s (clojure.string/split s #"x")]
+    (mapv #(Integer/parseInt %) split-s)))
+
+(defn parse
+  "Parse the input string and return a vector of vectors. Each vector
+  holds the gift dimensions as returned by parse-line."
+  [s]
+  (let [split-s (clojure.string/split-lines s)]
+    (mapv parse-line split-s)))
+
+(def gift-dimensions (parse (slurp input-file)))
+
+; --------------------------
+; results
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println gift-dimensions))
