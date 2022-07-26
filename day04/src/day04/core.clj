8f8ecac7b5cbf5db4804364a962848246b7047d6
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-07-26 16:08:44 +0300

    day4 p1,p2: compute solutions

--- day04/src/day04/core.clj
+++ day04/src/day04/core.clj
@@ -19,6 +19,35 @@
 
 (def input "bgvyzdsv")
 
+(defn candidate-strings
+  "Returns a lazy seq of the strings that will be examined.
+  The first part of the string is always the input.
+  The second part is a positive integer (1,2,...)"
+  []
+  (let [nums (map inc (range))]
+    (map #(str input %) nums)))
+
+(defn find-lowest-num
+  "Finds the lowest positive number that produces a hash that starts with substr.
+  Returns the number as a string."
+  [substr]
+  (let [strings (candidate-strings)]
+    (some #(and (clojure.string/starts-with? (md5 %) substr)
+                (apply str (drop (count input) %)))
+          strings)))
+
+; --------------------------
+; results
+
+(defn day04-1
+  []
+  (find-lowest-num "00000"))
+
+(defn day04-2
+  []
+  (find-lowest-num "000000"))
+
 (defn -main
   []
-  (println (md5 input)))
+  (println (day04-1))
+  (println (day04-2)))
