b2a414b8af94b16dca256ce7a9536e2faa148789
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-07-27 02:26:49 +0300

    day6: parse input

--- day06/src/day06/core.clj
+++ day06/src/day06/core.clj
@@ -1,7 +1,43 @@
 (ns day06.core
   (:gen-class))
 
+; --------------------------
+; common
+
+(def input-file "resources\\input.txt")
+
+(defn parse-loc
+  "Accepts a collection of strings and maps each element to an integer. Returns a vector."
+  [loc]
+  (mapv #(Integer/parseInt %) loc))
+
+(defn parse-line
+  "Parses an input line and returns a vector that consists of:
+  a keyword (:on :off :toggle) and two vectors that correspond to the
+  opposite corners of a rectangle."
+  [s]
+  (let [replaced-s (-> s
+                       (clojure.string/replace #"turn on " "~on~")
+                       (clojure.string/replace #"turn off " "~off~")
+                       (clojure.string/replace #"toggle " "~toggle~")
+                       (clojure.string/split #"~| through "))
+        [instr loc1 loc2] (->> replaced-s
+                               (filter (complement clojure.string/blank?))
+                               (map #(clojure.string/split % #",")))
+        instr-key (keyword (first instr))]
+    (vector instr-key (parse-loc loc1) (parse-loc loc2))))
+
+(defn parse
+  "Parses the input and returns a vector of instructions.
+  An instruction is represented by the data structure returned by parse-line."
+  [s]
+  (mapv parse-line (clojure.string/split-lines s)))
+
+(def instructions (parse (slurp input-file)))
+
+; --------------------------
+; results
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println instructions))
