462ac69205b9606b57455b86bea94e8671b92cef
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-07-26 14:03:59 +0300

    day3 p2: compute solution

--- day03/src/day03/core.clj
+++ day03/src/day03/core.clj
@@ -32,12 +32,32 @@
       house-locations)))
 
 ; --------------------------
+; problem 2
+
+(defn separate-instructions
+  "Splits the instructions into two vectors, first one is for santa,
+  second one is for robosanta."
+  [instructions]
+  (let [split-instructions (partition 2 instructions)
+        santa-instructions (mapv first split-instructions)
+        robosanta-instructions (mapv second split-instructions)]
+    [santa-instructions robosanta-instructions]))
+
+; --------------------------
 ; results
 
 (defn day03-1
   []
   (count (house-locations instructions)))
 
+(defn day03-2
+  []
+  (let [[santa-instructions robosanta-instructions] (separate-instructions instructions)
+        santa-houses (house-locations santa-instructions)
+        robosanta-houses (house-locations robosanta-instructions)]
+    (count (into santa-houses robosanta-houses))))
+
 (defn -main
   []
-  (println (day03-1)))
+  (println (day03-1))
+  (println (day03-2)))
