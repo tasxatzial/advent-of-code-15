8045b844555cfaac98a6944142a0452bdd7175c0
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-07-26 16:08:23 +0300

    day4: add md5 hash function

--- day04/src/day04/core.clj
+++ day04/src/day04/core.clj
@@ -1,12 +1,24 @@
 (ns day04.core
   (:gen-class))
 
+; md5 function: https://gist.github.com/jizhang/4325757
+
+(import 'java.security.MessageDigest
+        'java.math.BigInteger)
+
+(defn md5
+  [^String s]
+  (->> s
+       .getBytes
+       (.digest (MessageDigest/getInstance "MD5"))
+       (BigInteger. 1)
+       (format "%032x")))
+
 ; --------------------------
 ; common
 
 (def input "bgvyzdsv")
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  []
+  (println (md5 input)))
