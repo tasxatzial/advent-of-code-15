a1f8f9d1f352ad29eb5276ee9d1c941f8ccae7ea
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-07-26 13:48:26 +0300

    day3: find the locations of the houses that receive at least one present

--- day03/src/day03/core.clj
+++ day03/src/day03/core.clj
@@ -8,9 +8,32 @@
 
 (def instructions (slurp input-file))
 
+(defn next-house-location
+  "Finds the location of the next house based on the current location [x y]
+  and an instruction (^ > v <)."
+  [[x y] instruction]
+  (case instruction
+    \^ [x (inc y)]
+    \v [x (dec y)]
+    \> [(inc x) y]
+    \< [(dec x) y]))
+
+(defn house-locations
+  "Finds the locations of all houses that receive at least one present."
+  [instructions]
+  (loop [[instruction & rest-instructions] instructions
+         curr-location [0 0]
+         house-locations #{curr-location}]
+    (if instruction
+      (let [new-location (next-house-location curr-location instruction)]
+        (if (contains? house-locations new-location)
+          (recur rest-instructions new-location house-locations)
+          (recur rest-instructions new-location (conj house-locations new-location))))
+      house-locations)))
+
 ; --------------------------
 ; results
 
 (defn -main
   []
-  (println instructions))
+  (println (house-locations instructions)))
