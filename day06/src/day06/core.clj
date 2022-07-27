59c19e9f4f8ec0756f98bcb6f6b1fb8d1e0426c8
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2022-07-27 18:02:13 +0300

    day6: new faster p1 solution & refactoring

--- day06/src/day06/core.clj
+++ day06/src/day06/core.clj
@@ -42,17 +42,6 @@
   (and (>= x2 x0 x1)
        (>= y2 y0 y1)))
 
-(defn final-state
-  "Computes the final state of a light positioned at loc. New-state-fn is the
-  function that computes its next state."
-  [loc initial-state new-state-fn]
-  (loop [state initial-state
-         [instruction & rest-instructions] instructions]
-    (if instruction
-      (let [next-state (new-state-fn state loc instruction)]
-        (recur next-state rest-instructions))
-      state)))
-
 (defn grid
   "Returns a 1000x1000 grid. Coordinates start from [0 0] and go up to [999 999]."
   []
@@ -65,22 +54,36 @@
 ; --------------------------
 ; problem 1
 
-(defn new-state_p1
-  "Returns the new state of a light positioned at loc when it receives an instruction of
-  the form [cmd loc1 loc2] (problem 1)"
-  [state loc [cmd loc1 loc2]]
-  (if (is-included? loc loc1 loc2)
-    (if (= :toggle cmd)
-      (if (= :off state)
-        :on
-        :off)
-      cmd)
-    state))
+(def reversed-instructions (rseq instructions))
+
+(defn compute-state_p1
+  "Finds the final state of a light given its initial state and the number
+  of times its state has been toggled (problem 1)."
+  [initial-state toggle-count]
+  (if (even? toggle-count)
+    initial-state
+    (if (= :on initial-state)
+      :off
+      :on)))
+
+(defn final-state_p1
+  "Returns the final state of a light positioned at loc (problem 1)"
+  [loc]
+  (loop [[[cmd loc1 loc2] & rest-instructions] reversed-instructions
+         toggle-count 0]
+    (if cmd
+      (if (is-included? loc loc1 loc2)
+        (case cmd
+          :on (compute-state_p1 :on toggle-count)
+          :off (compute-state_p1 :off toggle-count)
+          :toggle (recur rest-instructions (inc toggle-count)))
+        (recur rest-instructions toggle-count))
+      (compute-state_p1 :off toggle-count))))
 
 ; --------------------------
 ; problem 2
 
-(defn new-state_p2
+(defn compute-state_p2
   "Returns the new state of a light positioned at loc when it receives an instruction of
   the form [cmd loc1 loc2] (problem 2)"
   [state loc [cmd loc1 loc2]]
@@ -91,23 +94,33 @@
       :off (max (dec state) 0))
     state))
 
+(defn final-state_p2
+  "Computes the final state of a light positioned at loc (problem 2)"
+  [loc initial-state]
+  (loop [state initial-state
+         [instruction & rest-instructions] instructions]
+    (if instruction
+      (let [next-state (compute-state_p2 state loc instruction)]
+        (recur next-state rest-instructions))
+      state)))
+
 ; --------------------------
 ; results
 
 (defn day06-1
   []
   (->> (memoized-grid)
-       (map #(final-state % :off new-state_p1))
+       (map #(final-state_p1 %))
        (filter #{:on})
        count))
 
 (defn day06-2
   []
   (->> (memoized-grid)
-       (map #(final-state % 0 new-state_p2))
+       (map #(final-state_p2 % 0))
        (apply +)))
 
 (defn -main
   []
-  (println (day06-1))
-  (println (day06-2)))
+  (println (time (day06-1)))
+  (println (time (day06-2))))
